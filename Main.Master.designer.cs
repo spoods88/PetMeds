a200057ce75298d25ff2bcf07be3921a55403e97
Nyla Rashied <tuh14505@temple.edu>
2021-10-19 12:21:14 -0400

    Added buttons

--- Main.Master.designer.cs
+++ Main.Master.designer.cs
@@ -33,6 +33,33 @@ namespace PetMeds
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// btnVet control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnVet;
+
+        /// <summary>
+        /// btnDispensing control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnDispensing;
+
+        /// <summary>
+        /// btnService control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnService;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
