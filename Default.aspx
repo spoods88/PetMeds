a77ce7def483a02e3688d36eb9fc3a56b186d6f8
Nyla Rashied <tuh14505@temple.edu>
2021-10-20 01:28:55 -0400

    added contents

--- Default.aspx
+++ Default.aspx
@@ -1,6 +1,11 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PetMeds.Default" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    Vet
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-
+    Dispening
 </asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    Service
+</asp:Content>
+
--- Main.Master.designer.cs
+++ Main.Master.designer.cs
@@ -67,5 +67,14 @@ namespace PetMeds
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+
+        /// <summary>
+        /// ContentPlaceHolder2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
     }
 }
